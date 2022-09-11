b3c6ab9de2b0c97f1d3aabfc2a95c9bd5e3de0b6
stephen <onwuegbuokwustephen@gmail.com>
2022-09-11 15:06:33 +0000

    program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main (void)
+int main(void)
 { 
-	char str[] = "Programming is like building a multilungual puzzle\n";
+	char str[] = ""Programming is like building a multilungual puzzle\n";
 	puts(str);
-	return (0);
+	return(0);
 }
