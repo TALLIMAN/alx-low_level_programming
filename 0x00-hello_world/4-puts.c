493097b0ca87f4c16a8886af8c9a1c0b78ecb535
stephen <onwuegbuokwustephen@gmail.com>
2022-09-11 15:47:58 +0000

    the program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main (void)
-{ 
-	puts ("Programming is like building a multilingual puzzle\n");
+{ 	
+	int str[] = "Programming is like building a multilingual puzzle\n"; 
+	puts(str); 
 	return (0);
 }
