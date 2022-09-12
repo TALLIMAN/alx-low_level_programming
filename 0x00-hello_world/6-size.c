bee427a1b694732fa3bde2af7df4abf69443defe
stephen <onwuegbuokwustephen@gmail.com>
2022-09-12 01:46:33 +0000

    the sizes of datatypes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,12 @@ int main(void)
 	char c;
 	long int lu;
 	long long int lld;
+	float f;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(lu));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long long int)sizeof(lld));
-
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lld));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f);
 	return (0);
 }
