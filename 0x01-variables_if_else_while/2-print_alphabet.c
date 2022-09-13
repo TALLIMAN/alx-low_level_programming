22ebf553ce5ffcce3519b762f2b8ddf4bee24a44
stephen <onwuegbuokwustephen@gmail.com>
2022-09-13 00:28:34 +0000

    the alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- a value is returned in putchar
+ * Return: the return value is 0
+ */
+int main(void)
+
+	for (cap = 'a'; cap <= 'z'; cap++)
+		putchar(cap);
+	return (0);
