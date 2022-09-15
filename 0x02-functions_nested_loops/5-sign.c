72077323417441b905f796f440030333709e886c
stephen <onwuegbuokwustephen@gmail.com>
2022-09-15 15:46:05 +0000

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print_sign - this function prints the sign and returns the number
+ * based on their sign
+ *
+ * Return: the value of the return is the result
+ *
+ * @n: the datatype of the parameter is the same to match the datatype
+ * of the value its been called to and also the return type is int to
+ * match the datatype of the returned value.
+ *
+ */
+
+int print_sign(int n)
+{
+	int result;
+
+	if (n > 0)
+	{
+		putchar('+');
+		putchar(' ');
+		result = 1;
+	} else if (n == 0)
+	{
+		putchar('0');
+		putchar(' ');
+		result = 0;
+	} else
+	{
+		if (n < 0)
+		{
+			putchar('-');
+			putchar(' ');
+			result = -1;
+		}
+	}
+	return (result);
+}
