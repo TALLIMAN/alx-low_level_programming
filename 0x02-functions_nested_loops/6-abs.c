22f954a9a40d07d7eb20149f64bd5c7514feab31
stephen <onwuegbuokwustephen@gmail.com>
2022-09-15 16:38:31 +0000

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - the function returns the absolute value of the parameter
+ * from where it is been called
+ *
+ * Return: the return value is the result obtained by the data type
+ * of the parameter
+ *
+ * @int: the datatype of the parameter
+ *
+ */
+int _abs(int)
+{
+	int result;
+
+	if (int >= '0' || int <= '0')
+	{
+		result = int;
+	}
+	return (result);
+}
