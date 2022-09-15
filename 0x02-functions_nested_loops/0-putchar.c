d5fdfba77807cd9e82428fd92ccadeceaf2a431d
stephen <onwuegbuokwustephen@gmail.com>
2022-09-15 00:33:31 +0000

    the putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- the value returned to the main function is 0
+ * Return: the value of zero is returned
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
