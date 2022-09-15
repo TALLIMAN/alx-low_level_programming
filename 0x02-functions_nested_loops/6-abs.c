c166498db4eb4766c4a5311cacaaed3b319ae082
stephen <onwuegbuokwustephen@gmail.com>
2022-09-15 21:34:43 +0000

    the highest value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,24 +6,24 @@
  * Return: the return value is the result obtained by the data type
  * of the parameter
  *
- * @j: the datatype of the parameter is int
+ * @num: the datatype of the parameter is int
  *
  */
-int _abs(int j)
+int _abs(int num)
 {
 	int result;
 
-	if (j > '0')
+	if (num > '0')
 	{
-		result = j;
-	} else if (j == '0')
+		result = num;
+	} else if (num == '0')
 	{
 		result = 0;
 	} else
 	{
-		if (j < '0')
+		if (num < '0')
 		{
-			result = j * -1;
+			result = num * -1;
 		}
 	}
 	return (result);
