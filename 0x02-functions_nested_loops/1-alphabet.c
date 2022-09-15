9ad554b4361f8ffd967cd897463486b6fed45569
stephen <onwuegbuokwustephen@gmail.com>
2022-09-15 11:03:00 +0000

    the lowercase alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,8 @@
 #include "main.h"
 /**
- * main- check the code...the value should return zero
- *
- * Return: return value of zero
- *
- * print_alphabet(void)- check the code...no returned value
+ * print_alphabet - print all the alphabets and a new line
  *
  */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 
 void print_alphabet(void) /*the function for defining the loop for alphabets*/
 {
