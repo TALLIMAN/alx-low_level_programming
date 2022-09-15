573e4f9d507721e9e48af612e01f820ca2f60c5a
stephen <onwuegbuokwustephen@gmail.com>
2022-09-15 21:08:33 +0000

    the absolute number

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,16 +6,22 @@
  * Return: the return value is the result obtained by the data type
  * of the parameter
  *
- * @int: the datatype of the parameter
+ * @j: the datatype of the parameter is int
  *
  */
 int _abs(int j)
 {
 	int result;
 
-	if (j >= '0' || j <= '0')
+	if (j >= '0')
 	{
 		result = +j;
+	} else 
+	{
+		if (j <= '0')
+		{
+			result = j * -1;
+		}
 	}
 	return (result);
 }
