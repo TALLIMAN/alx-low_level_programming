b04d682f2614f8c282ba0650781880472de1af43
stephen <onwuegbuokwustephen@gmail.com>
2022-09-21 09:09:22 +0000

    the string printing

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_puts - this function prints the string of the parameter
+ *
+ * @str: this is the parameter used for dereferencing
+ */
+
+void _puts(char *str)
+{
+	while (*str++)
+	{
+		putchar(*str++);
+	}
+}
