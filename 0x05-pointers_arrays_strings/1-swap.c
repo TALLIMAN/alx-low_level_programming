b52f46b19fe0a42f1e805287f9c56fae211f532a
stephen <onwuegbuokwustephen@gmail.com>
2022-09-21 00:49:57 +0000

    the swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
 /**
+ *swap_int - this function swaps the numbers
  *
- *
+ * @a: the integer to be swapped
+ * @b: another integer to be swapped
  */
 
 void swap_int(int *a, int *b)
 {
-	*b = 98;
-	*a = 42;
+	int cap = *a;
+	*a = *b;
+	*b = cap;
 }
